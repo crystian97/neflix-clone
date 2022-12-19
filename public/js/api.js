e6a0ccfe36a4063fed5dd4acede610f8544cfd4c
crystian97 <icrystianveloso@gmail.com>
2022-12-19 17:29:51 -0300

    feat:config imdb api keys

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = '8bca2bc722d4582663f95312666c8f48'
+let img_url = 'https://image.tmdb.org/t/p/w500'
+let genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?'
+let movie_genres_http = 'https://api.themoviedb.org/3/discover/movie?'
